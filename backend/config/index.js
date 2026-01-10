fc02fcd66ff7bdc4cf00b8925de505addbe1473a
Vishal Singh <vishalu.cifer@gmail.com>
2026-01-10 19:11:13 +0530

    transformed frontend from next.js to react+vite

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,27 +2,22 @@ const express = require('express');
 const userRouter = require('../routes/User')
 const messageRouter = require('../routes/message')
 const connectToMongo = require('./db')
-const next = require('next');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const cloudinary = require('cloudinary').v2;
+require('dotenv').config()
 
 const app = express();
 const dev = process.env.NODE_ENV !== 'production';
-const nextApp = next({ dev, dir: path.join(__dirname,"../../frontend/")});
-const handle = nextApp.getRequestHandler();
 cloudinary.config({
-  secure: process.env.NODE_ENV=='production'?true:false,
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.CLOUD_API_KEY, 
-  api_secret: process.env.CLOUD_API_SECRET
+    secure: process.env.NODE_ENV === 'production', cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
 });
 
 (async () => {
     try {
         await connectToMongo();
-        await nextApp.prepare();
-        console.log("Next.js prepared");
 
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
@@ -30,9 +25,6 @@ cloudinary.config({
 
         app.use('/api/v1/auth', userRouter)
         app.use('/api/v1/msg', messageRouter)
-        app.use((req, res) => {
-            return handle(req, res);
-        });
 
         app.use((error, req, res, next) => {
             res.status(500).json({
@@ -41,6 +33,15 @@ cloudinary.config({
             });
         });
         const PORT = process.env.PORT;
+        if (process.env.NODE_ENV === 'production') {
+            app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+            app.use((req, res, next) => {
+                res.sendFile(
+                    path.join(__dirname, '../../frontend/dist/index.html')
+                );
+            });
+        }
         app.listen(PORT || 3000, () => {
             console.log(`Listening at port ${PORT || 3000}`)
         });
